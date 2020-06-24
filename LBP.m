b84ca564c67d048ad15866540955d580692e1424
Reza ADB <adbrezaooo@gmail.com>
2020-06-24 23:21:47 +0430

    added some minor improvements

--- LBP.m
+++ LBP.m
@@ -2,19 +2,19 @@ function LBP()
     % %     read Class1 Training images.
     class1 = dir('Class1 Training images\\*.jpg');
     class1Size = numel(class1);
-    Class1LBPs = zeros(class1Size, 59);
+    class1LBPs = zeros(class1Size, 59);
     for i = 1:class1Size
         input = rgb2gray(imread([class1(i).folder, '\', class1(i).name]));
-        Class1LBPs(i,:) = extractLBPFeatures(input);
+        class1LBPs(i,:) = extractLBPFeatures(input);
         disp(['i: ', num2str(i)]);    
     end
     % %     read Class2 Training images.
     class2 = dir('Class2 Training images\\*.jpg');
     class2Size = numel(class2);
-    Class2LBPs = zeros(class2Size,  59);
+    class2LBPs = zeros(class2Size,  59);
     for j = 1:class2Size
         input = rgb2gray(imread([class2(j).folder, '\', class2(j).name]));
-        Class2LBPs(j,:) = extractLBPFeatures(input);
+        class2LBPs(j,:) = extractLBPFeatures(input);
         disp(['j: ', num2str(j)]);
     end
     clearvars i j input class1 class1Size class2 class2Size
