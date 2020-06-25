b18f619918409335321c2c5a856e073b529f8080
Reza ADB <adbrezaooo@gmail.com>
2020-06-25 21:20:54 +0430

    Now Train Classifiers are more dynamic.

--- getDistance.m
+++ getDistance.m
@@ -8,6 +8,8 @@ c2s = size(class2HOGs);
 c1s = c1s(1);
 c2s = c2s(1);
 %% get training models
+% go to "training functions" directory to use functions
+cd 'Training Functions'
 HOGs = class1HOGs;
 HOGs(c1s + 1:c1s + c2s, :) = class2HOGs;
 HOGs(1:c1s,binCount * cellSize ^ 2 + 1) = 1;
@@ -20,6 +22,8 @@ knnHOG = KNNTrainClassifierForHOG(HOGs);
 svmHOG = SVMTrainClassifierForHOG(HOGs);
 knnLBP = KNNTrainClassifierForLBP(LBPs);
 svmLBP = SVMTrainClassifierForLBP(LBPs);
+% go back to main directory
+cd ..
 %% get test pics
 testImages = dir('test\\*.jpg');
 testImagesSize = numel(testImages);
@@ -35,4 +39,4 @@ for i=1:testImagesSize
     class(i,3) = knnLBP.predictFcn(LBP);
     class(i,4) = svmLBP.predictFcn(LBP);
 end
-clearvars binCount cellSize i input testImages testImagesSize
+clearvars binCount cellSize i input testImages testImagesSize c1s c2s class1HOGs class1LBPs class2HOGs class2LBPs HOG HOGs knnHOG knnLBP LBP LBPs svmHOG svmLBP
